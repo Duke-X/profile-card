ced01c3983111774b6c464abf8abe8e898421850
Vadish Chhatwal <vadishchhatwal04@gmail.com>
2025-02-03 19:17:01 +0530

    Created a card & added my pic

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//<StrictMode> - It helps you catch potential issues in your website, but it doesn't render anything in UI
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode>               
     <App />
   </StrictMode>,
 )
